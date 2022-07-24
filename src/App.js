8d184a2608062584c60f7404eab18948daf718c4
Monik Pamecha <monik@etiole.com>
2022-07-24 11:33:37 -0400

    add,revoke, view hashes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TronWeb from 'tronweb';
 import {useEffect} from "react";
+import utils from "./utils/wallet";
 
 function App() {
   const fullNode = 'https://api.shasta.trongrid.io';
   const solidityNode = 'https://api.shasta.trongrid.io';
   const eventServer = 'https://api.shasta.trongrid.io';
-  const privateKey = 'xxx';
-  const tronWeb = new TronWeb(fullNode,solidityNode,eventServer);
-  const publicAddress = window.tronWeb.defaultAddress.base58
+  let tronWeb = new TronWeb(fullNode,solidityNode,eventServer, '');
+  const [publicAddress, setPublicAddress] = useState('')
+
+  function runHashes() {
+    utils.runHashes();
+  }
+
+  function issueCredential() {
+    let cred = utils.issueCredential("test2", publicAddress);
+    console.log(cred);
+  }
+
+  function revokeCredential() {
+    let cred = utils.revokeCredential("test2");
+    console.log(cred);
+  }
 
   useEffect(() => {
-
+    if(window.tronWeb) {
+      tronWeb = window.tronWeb;
+      setPublicAddress(window.tronWeb.defaultAddress.base58);
+      utils.setContract(tronWeb);
+    }
   });
+
+  //issue credential
+  //revoke credential
+  //view hashes
+
 
   return (
     <div className="App">
@@ -25,6 +49,9 @@ function App() {
         <p>
           Your address is {publicAddress}
         </p>
+        <button onClick={runHashes}>Run Hashes</button>
+        <button onClick={issueCredential}>Issue Credential</button>
+        <button onClick={revokeCredential}>Revoke Credential</button>
       </header>
     </div>
   );
---
+++ src/utils/wallet.js
@@ -0,0 +1,139 @@
+const utils = {
+    tronWeb: false,
+    contract: false,
+    abi: [
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "string",
+                    "name": "_hash",
+                    "type": "string"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "issuer",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                }
+            ],
+            "name": "CredentialIssued",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "string",
+                    "name": "_hash",
+                    "type": "string"
+                }
+            ],
+            "name": "CredentialRevoked",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "name": "hashes",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "issuer",
+                    "type": "address"
+                },
+                {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                },
+                {
+                    "internalType": "enum CredentialStore.State",
+                    "name": "status",
+                    "type": "uint8"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_hash",
+                    "type": "string"
+                },
+                {
+                    "internalType": "address",
+                    "name": "recipient",
+                    "type": "address"
+                }
+            ],
+            "name": "issue_credential",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_hash",
+                    "type": "string"
+                }
+            ],
+            "name": "revoke_credential",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ],
+    contract_address: "TJRLpvvkiEfhYy8HSdPjYza3EuSxSY3bmu",
+
+    async setContract(tronWeb) {
+        this.contract = await tronWeb.contract(this.abi, this.contract_address);
+    },
+
+    async runHashes() {
+        let h = await this.contract.hashes('test2').call();
+        console.log(h);
+    },
+
+    async issueCredential(_hash, recipient) {
+        let h = await this.contract.issue_credential(_hash, recipient).send({
+            feeLimit:100_000_000,
+            callValue: 0,
+            shouldPollResponse:true
+        });
+        return h;
+    },
+
+    async revokeCredential(_hash) {
+        let h = await this.contract.revoke_credential(_hash).send({
+            feeLimit:100_000_000,
+            callValue: 0,
+            shouldPollResponse:true
+        });
+        return h;
+    }
+};
+
+export default utils;
