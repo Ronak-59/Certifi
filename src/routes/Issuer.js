02a12de88db588ec10e01494629281d800eea184
Monik Pamecha <monik@etiole.com>
2022-07-25 12:57:17 -0400

    fixed header

--- src/routes/Issuer.js
+++ src/routes/Issuer.js
@@ -16,6 +16,7 @@ let tronWeb = new TronWeb(fullNode,solidityNode,eventServer, '');
 
 export function Issuer(props) {
     const [publicAddress, setPublicAddress] = useState('');
+    const [currentlyVieweing, setCurrentlyViewing] = useState(0);
 
     useEffect(() => {
         if(window.tronWeb) {
@@ -28,9 +29,17 @@ export function Issuer(props) {
 
     return (
         <div>
-            <h1> Issuer </h1>
-            <p><Link to="issue-credentials">Issue Credentials</Link></p>
-            <p><Link to="view-credentials">View Credentials</Link></p>
+            <h1> For Institutions </h1>
+            <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+                <li className="mr-2">
+                    <Link to="issue-credentials" aria-current="page"
+                       className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Profile</Link>
+                </li>
+                <li className="mr-2">
+                    <Link to="view-credentials"
+                       className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Dashboard</Link>
+                </li>
+            </ul>
 
             <Outlet />
 
